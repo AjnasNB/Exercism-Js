debe8f680c3d2c23cce0da0da89eb2e5edb0bd80
AjnasNB <ajnasnb@gmail.com>
2023-06-06 23:39:03 +0530

    next commit

--- lasagna/lasagna.js
+++ lasagna/lasagna.js
@@ -1,10 +1,12 @@
 export const EXPECTED_MINUTES_IN_OVEN=40
+const PREPARATION_MINUTES_PER_LAYER=2
 export function remainingMinutesInOven(actualMinutesInOven){
   return EXPECTED_MINUTES_IN_OVEN-actualMinutesInOven
 }
 export function preparationTimeInMinutes(numberOfLayers){
-  return numberOfLayers*2
+  return numberOfLayers*PREPARATION_MINUTES_PER_LAYER
 }
+
 export function totalTimeInMinutes(numberOfLayers,actualMinutesInOven){
   return preparationTimeInMinutes(numberOfLayers)+actualMinutesInOven
 }
