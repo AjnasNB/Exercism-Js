35db45a8f869ead477aadd87efa4870514b1355a
AjnasNB <ajnasnb@gmail.com>
2023-06-07 23:20:49 +0530

    new question 5 door-policy

--- poetry-club-door-policy/door-policy.js
+++ poetry-club-door-policy/door-policy.js
@@ -1,3 +1,22 @@
-export function frontDoorResponse(){
-  
+export function frontDoorResponse(poemLine){
+  return poemLine[0];
+
 }
+
+export function frontDoorPassword(poemWord){
+  
+  poemWord=poemWord.toLowerCase();
+   
+   return poemWord[0].toUpperCase()+poemWord.slice(1);
+  }
+
+  export function backDoorResponse(poemLine){
+   poemLine=poemLine.trim();
+    return poemLine[poemLine.length-1];
+  
+  }
+
+  export function backDoorPassword(poemWord){
+
+    return frontDoorPassword(poemWord)+", please";
+  }
