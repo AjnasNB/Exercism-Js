ad9f1c4758cf285e84d20c25ab22026c8b0e8da3
AjnasNB <ajnasnb@gmail.com>
2023-06-10 23:40:01 +0530

    nrew

--- coordinate-transformation/coordinate-transformation.js
+++ coordinate-transformation/coordinate-transformation.js
@@ -15,7 +15,9 @@
  *  translated coordinate pair in the form [x, y]
  */
 export function translate2d(dx, dy) {
-  throw new Error('Implement the translate2d function');
+  return function(x,y){
+    return [x+dx,y+dy];
+  }
 }
 
 /**
@@ -29,7 +31,10 @@ export function translate2d(dx, dy) {
  *  scaled coordinate pair in the form [x, y]
  */
 export function scale2d(sx, sy) {
-  throw new Error('Implement the scale2d function');
+
+  return function(x,y){
+    return [x*sx,y*sy];
+  }
 }
 
 /**
@@ -43,7 +48,11 @@ export function scale2d(sx, sy) {
  *  transformed coordinate pair in the form [x, y]
  */
 export function composeTransform(f, g) {
-  throw new Error('Implement the composeTransform function');
+  return function(x,y){
+    return g(f(x,y)[0],f(x,y)[1]);
+  
+
+  }
 }
 
 /**
@@ -56,5 +65,20 @@ export function composeTransform(f, g) {
  *  if the arguments are the same on subsequent calls, or compute a new result if they are different.
  */
 export function memoizeTransform(f) {
-  throw new Error('Implement the memoizeTransform function');
+
+  let lastX = null;
+  let lastY = null;
+  let lastResult = null;
+
+  return function(x,y){
+    if(lastX === x && lastY === y){
+      return lastResult;
+    }
+    else{
+      lastX = x;
+      lastY = y;
+      lastResult = f(x,y);
+      return lastResult;
+    }
+  }
 }
