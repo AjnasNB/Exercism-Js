4fa840f82f3b9d928fb4c4b8636271d7ec9ce1a2
AjnasNB <ajnasnb@gmail.com>
2023-06-14 20:41:59 +0530

    get new 22

---
+++ factory-sensors/factory-sensors.js
@@ -0,0 +1,53 @@
+// @ts-check
+
+export class ArgumentError extends Error {}
+
+export class OverheatingError extends Error {
+  constructor(temperature) {
+    super(`The temperature is ${temperature} ! Overheating !`);
+    this.temperature = temperature;
+  }
+}
+
+/**
+ * Check if the humidity level is not too high.
+ *
+ * @param {number} humidityPercentage
+ * @throws {Error}
+ */
+export function checkHumidityLevel(humidityPercentage) {
+  if (humidityPercentage > 70) {
+    throw new Error('Humidity level too high !');
+
+  }
+}
+
+/**
+ * Check if the temperature is not too high.
+ *
+ * @param {number|null} temperature
+ * @throws {ArgumentError|OverheatingError}
+ */
+export function reportOverheating(temperature) {
+  if (temperature === null) {
+    throw new ArgumentError('No temperature given !');
+  }
+  if (temperature > 500) {
+    throw new OverheatingError(temperature);
+  }
+}
+
+/**
+ *  Triggers the needed action depending on the result of the machine check.
+ *
+ * @param {{
+ * check: function,
+ * alertDeadSensor: function,
+ * alertOverheating: function,
+ * shutdown: function
+ * }} actions
+ * @throws {ArgumentError|OverheatingError|Error}
+ */
+export function monitorTheMachine(actions) {
+  throw new Error('Implement the monitorTheMachine function');
+}
