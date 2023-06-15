25962143e5f5baf2093eac6358cd13285ca90fed
AjnasNB <ajnasnb@gmail.com>
2023-06-15 08:16:46 +0530

    commit

--- custom-signs/custom-signs.js
+++ custom-signs/custom-signs.js
@@ -14,7 +14,8 @@
  */
 
 export function buildSign(occasion, name) {
-  throw new Error('Implement the buildSign function');
+  return `Happy ${occasion} ${name}!`
+  
 }
 
 /**
@@ -26,7 +27,7 @@ export function buildSign(occasion, name) {
  */
 
 export function buildBirthdaySign(age) {
-  throw new Error('Implement the buildBirthdaySign function');
+  return `Happy Birthday! What a ${age>=50?"mature":"young"} fellow you are.`
 }
 
 /**
@@ -39,7 +40,7 @@ export function buildBirthdaySign(age) {
  */
 
 export function graduationFor(name, year) {
-  throw new Error('Implement the graduationForm function');
+  return `Congratulations ${name}!${"\n"}Class of ${year}`
 }
 
 /**
@@ -53,5 +54,5 @@ export function graduationFor(name, year) {
  */
 
 export function costOf(sign, currency) {
-  throw new Error('Implement the costOf function');
+  return `Your sign costs ${((sign.length*2+20)).toFixed(2)} ${currency}.`
 }
