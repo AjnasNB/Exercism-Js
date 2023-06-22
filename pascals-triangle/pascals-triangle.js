6c9a48a3a847815413466b7bc38995c79f106732
AjnasNB <ajnasnb@gmail.com>
2023-06-23 01:00:18 +0530

    pascals triangle

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -3,6 +3,16 @@
 // convenience to get you started writing code faster.
 //
 
-export const rows = () => {
-  throw new Error('Remove this statement and implement this function');
+export const rows = (rowNumber) => {
+  let triangle=[]
+  for (let i=0; i<rowNumber; i++){
+    triangle[i] = []
+    triangle[i][0] = triangle[i][i] = 1  
+    for (let j=1; j<i; j++){
+      triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+    }
+    
+  }
+  return triangle
+  
 };
