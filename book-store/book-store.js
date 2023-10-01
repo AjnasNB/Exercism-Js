f0c8f1047dc95f63d310e026185f60b4b10d8468
AjnasNB <ajnasnb@gmail.com>
2023-10-01 17:54:46 +0530

    upload

--- book-store/book-store.js
+++ book-store/book-store.js
@@ -1,39 +1,42 @@
-//
-// This is only a SKELETON file for the 'BookStore' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const cost = (books) => {
-  let cost=0;
-  let bookSet=[];
-  let discount={
-    1:1,
-    2:0.95,
-    3:0.9,
-    4:0.8,
-    5:0.75
-  }
+  let cost = 0;
+  let bookSet = [];
+  let discount = {
+    1: 1,
+    2: 0.95,
+    3: 0.9,
+    4: 0.8,
+    5: 0.75,
+  };
 
-  books.forEach(book => {
+  books.forEach((book) => {
     let addedToExistingSet = false;
     for (const set of bookSet) {
-      if (!set.includes(book) ) {
+      if (!set.includes(book)) {
         set.push(book);
         addedToExistingSet = true;
         break;
       }
     }
-    if (!addedToExistingSet ) {
+    if (!addedToExistingSet) {
       bookSet.push([book]);
     }
-  }
-  );
-  
-  
-
-  bookSet.forEach(set => {
-    cost+=set.length*800*discount[set.length];
   });
-return cost;
+  if(books.length>1 && books.length%8==0 ){
+    bookSet.forEach((set) => {
+      let set1 = bookSet[bookSet.length-1];
+      if(set.length>4&&set1.length<4){
+        set1.push(set.pop());
+      }
+      
+    });
+  }
 
-}
+
+  bookSet.forEach((set) => {
+    cost += set.length * 800 * discount[set.length];
+  });
+
+
+  return cost;
+};
